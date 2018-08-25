cbe30bc28f88025cb93eb4328d43f9ee2abd0625
HumYaiJang <39524873+HumYaiJang@users.noreply.github.com>
2018-08-25 14:21:07 +0000

    Update Main.php

--- src/HumCore/Main.php
+++ src/HumCore/Main.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace HumCore;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use jojoe77777\FormAPI;
+
+Class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->saveDefaultConfig();
+    $this->getServer()->registerEvents(($this), $this))
+}
